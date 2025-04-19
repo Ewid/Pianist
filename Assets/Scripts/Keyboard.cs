94bb7aceb4b214c6c92a355094986b731bc46a28
Youssef Ashraf Ewida <ye2101125@tkh.edu.eg>
2025-04-20 01:59:57 +0200

    Creating Images For The Piano, And Initial Scripts

---
+++ Assets/Scripts/Keyboard.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyboard : MonoBehaviour
+{
+    public GameObject blackKey, whiteKey;
+    
+    public GameObject content;
+
+    public int numberOfOctaves;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+}
